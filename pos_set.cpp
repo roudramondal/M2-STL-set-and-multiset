ba96091686915598df97b275f38f109bc7bd0c89
Code__R <103403881+roudramondal@users.noreply.github.com>
2025-05-20 23:55:00 +0600

    M2 pos int set

---
+++ pos_set.cpp
@@ -0,0 +1,48 @@
+/* Author: Code__R
+   Email: mondalroudra212@gmail.com
+   Date  : 05 / 18 / 2025
+   Time  : 00:31
+   Problem Link: 
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define nl '\n'
+#define YES cout << 'YES\n'
+#define Yes cout << 'Yes\n'
+#define NO cout << 'NO\n'
+#define No cout << 'No\n'
+#define ld long double
+#define pb push_back
+#define po pop_back
+#define all(v) v.begin(), v.end()
+
+#define fast_io ios::sync_with_stdio(false); cin.tie(NULL);
+
+void solve() {
+    // code here 
+    set<int>s;
+    s.insert(1);
+    s.insert(2);
+    s.insert(5);
+    s.insert(3);
+    set<int>::iterator pos = s.find(3);// if something is not found then it will retun set end
+     s.erase(pos);
+     for (auto x:s)
+     {
+        cout<<x<<" " ;
+     }
+     cout<<nl;
+}
+
+int main() {
+    //fast_io;
+    int t;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
