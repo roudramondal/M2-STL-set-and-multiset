aa0c95b412ee344fa6d70567f43c0c16e6b2bb88
Code__R <103403881+roudramondal@users.noreply.github.com>
2025-05-20 22:54:57 +0600

    M2 set

---
+++ find_set.cpp
@@ -0,0 +1,50 @@
+/* Author: Code__R
+   Email: mondalroudra212@gmail.com
+   Date  : 05 / 18 / 2025
+   Time  : 00:31
+   Problem Link: 
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define nl '\n'
+#define YES cout << 'YES\n'
+#define Yes cout << 'Yes\n'
+#define NO cout << 'NO\n'
+#define No cout << 'No\n'
+#define ld long double
+#define pb push_back
+#define po pop_back
+#define all(v) v.begin(), v.end()
+
+#define fast_io ios::sync_with_stdio(false); cin.tie(NULL);
+
+void solve() {
+    // code here 
+    vector<int>v;
+    set<int>s;
+    s.insert(1);
+    s.insert(-2);
+    s.insert(-4);
+    s.insert(-1);
+    s.insert(4);
+    cout<<*s.find(-4)<<nl;//point here
+    cout<<s.size()<<nl;
+    for (auto &x:s)
+    {
+        cout<<x<<" ";
+    }
+    cout<<nl;
+}
+
+int main() {
+    //fast_io;
+    int t;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
